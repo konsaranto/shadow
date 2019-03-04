b2ad2a60f53fee0c992e3a50b0857ee6d9319873
Kostas <konsaranto@gmail.com>
2019-03-04 22:43:44 +0200

    update

--- shadow.py
+++ shadow.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 '''
-Copyright © 2019 Konstantinos Sarantopoulos
+Copyright 2019 Konstantinos Sarantopoulos
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
